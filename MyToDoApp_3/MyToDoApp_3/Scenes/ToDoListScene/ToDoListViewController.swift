110fb5f85d3a4711c4e8d782eca7df02a69f6f54
= <=>
2023-09-13 16:02:05 +0900

    [FEAT] ToDoList View 구현

--- MyToDoApp_3/MyToDoApp_3/Scenes/ToDoListScene/ToDoListViewController.swift
+++ MyToDoApp_3/MyToDoApp_3/Scenes/ToDoListScene/ToDoListViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ToDoListViewController: UIViewController {
 
+    private let viewModel = TodoListViewModel()
+    
+    private let myTableview = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUp()
+    }
+    
+}
+
+private extension ToDoListViewController{
+    
+    func setUp(){
+        view.backgroundColor = .systemBackground
+        setUpTableView()
+    }
+    
+    func setUpTableView(){
+        view.addSubview(myTableview)
+        myTableview.dataSource = self
+        myTableview.delegate = self
+        myTableview.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
     
 }
---
+++ MyToDoApp_3/MyToDoApp_3/Scenes/ToDoListScene/ToDoListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoListViewModel.swift
+//  MyToDoApp_3
+//
+//  Created by SeoJunYoung on 2023/09/13.
+//
+
+import Foundation
+
+
+final class TodoListViewModel{
+    
+}

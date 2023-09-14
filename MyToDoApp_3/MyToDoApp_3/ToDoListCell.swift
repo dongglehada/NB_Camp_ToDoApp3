6d766028dd491b6cfb4324fee7914b1f82e885c4
= <=>
2023-09-14 16:10:24 +0900

    [FEAT] Cell 추가

---
+++ MyToDoApp_3/MyToDoApp_3/ToDoListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoListCell.swift
+//  MyToDoApp_3
+//
+//  Created by SeoJunYoung on 2023/09/14.
+//
+
+import UIKit
+
+class ToDoListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
